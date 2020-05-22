f0cfa32fe288d09e621fe2006bf02a1f747d43e0
Zhang xuncheng <chrischang110@gmail.com>
2020-05-21 20:01:37 -0400

    first commit

---
+++ tester/R/main.R
@@ -0,0 +1,34 @@
+
+#' @title Obtained Interarrival Time from a simulated Poisson Process
+#'
+#' @description This function provides functionality for obtaining interarrival time
+#' from a simulated Poisson Process.
+#'
+#' @param seed A user determined seed to randomly generate Poisson process
+#' @param t time of Poisson Process
+#' @param lambda lambda of Poisson Process
+#' @param p probability of the truncated Poisson
+#' @return Interarrival time from the simulated Poisson Process
+#' @export
+#'
+#' @examples main(seed=723,t=12,lambda=2,p=1)
+
+main <- function(seed=723, t=24, lambda = 2, p = 1/3) {
+  set.seed(seed)
+
+  N = rpois(1,lambda*t*p)
+
+  unifs = runif(N,0,t)
+  arrivals = sort(unifs)
+  difference = arrivals
+  for(i in 1:N){
+    if(i==1){
+      difference[i]=arrivals[i]
+    }
+    else{
+      difference[i]=arrivals[i]-arrivals[i-1]
+    }
+  }
+  return(difference)
+}
+
---
+++ tester/README.md
@@ -0,0 +1,28 @@
+
+# Interarrival
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of interarrival is to obtain Interarrival Time from a simulated Poisson Process
+
+## Installation
+
+You can install the released version of interarrival from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("interarrival")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(interarrival)
+## basic example code
+main(seed=123,t=24, lambda = 2, p = 1/3)
+```
+
+
+
